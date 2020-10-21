cd1ba32d7b359a702a2d139dc3b187a7defa47ff
AdrianMargel <carbongrouplife@gmail.com>
2020-10-21 07:09:18 -0600

    improved bot AI

--- bot/bot.js
+++ bot/bot.js
@@ -15,11 +15,34 @@ class Bot{
 		};
 
 		this.selfImage=new Player(this.stats);
+
+		this.time=0;
+		this.moveTo=new Vector(0,0);
 	}
 	control(controllingPlayer,visibleState,settings){
-		let move=new Vector();
-		let aim=new Vector(10,10);
-		let attack=true;
+		if(this.time%40==0){
+			this.moveTo=new Vector((Math.random()*2-1)*settings.size.x,(Math.random()*2-1)*settings.size.y);
+		}
+		this.time++;
+		let pos=new Vector(controllingPlayer.pos);
+		let dist=-1;
+		let closest=null;
+		for(let i=0;i<visibleState.playersList.length;i++){
+			if(visibleState.playersList[i].uid!=controllingPlayer.uid){
+				let pPos=new Vector(visibleState.playersList[i].pos);
+				let pDist=pos.getMag(pPos);
+				if(dist==-1||pDist<dist){
+					closest=visibleState.playersList[i];
+					dist=pDist;
+				}
+			}
+		}
+		let aim=new Vector(0,0);
+		if(closest!=null){
+			aim=new Vector(closest.pos);
+		}
+		let move=new Vector(this.moveTo);
+		let attack=closest!=null;
 		return new ControlRequest(move,aim,attack);
 	}
 	spawn(){
